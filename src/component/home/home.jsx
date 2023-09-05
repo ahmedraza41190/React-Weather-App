b0a36aae05aa8c194be82d2876b031b3969d40fb
Ahmed Raza <ragix.aemi@gmail.com>
2023-09-05 14:25:38 -0700

    Almost complete

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -1,7 +1,7 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import WeatherCard from "../weatherWidget/weatherWidget";
-import './home.css'
+import "./home.css"
 
 const Home = () => {
   // not recommended
@@ -10,32 +10,74 @@ const Home = () => {
   const [weatherData, setWeatherData] = useState([]);
   const cityNameRef = useRef(null);
 
+  const [currentLocationWeather, setCurrentLocationWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() => {
+    setIsLoading(true);
+    
+    const controller = new AbortController();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (location) => {
+        console.log("location: ", location);
+
+        try {
+          let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY}&units=metric`,
+            {
+              signal: controller.signal,
+            }
+          );
+          console.log(response.data);
+
+          setCurrentLocationWeather(response.data);
+          setIsLoading(false);
+        } catch (error) {
+          console.log(error.data);
+          setIsLoading(false);
+        }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      // cleanup function
+      controller.abort();
+    };
+  }, []);
+
   const submitHandler = async (e) => {
     e.preventDefault();
     console.log("cityName: ", cityNameRef.current.value);
 
     let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
     try {
+      setIsLoading(true);
+
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityNameRef.current.value}&appid=${API_KEY}&units=metric`
       );
 
       console.log(response.data);
       setWeatherData([response.data, ...weatherData]);
+      setIsLoading(false);
     } catch (error) {
       // handle error
-      console.log(error.data);
+      console.log(error?.data);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div >
-      <div className="inputCard">
-      <form  onSubmit={submitHandler}>
+    <div className="d">
+      <form onSubmit={submitHandler} className="inputCard">
+        <label htmlFor="cityNameInput"></label>
         <input
-        className="search-bar"
-          placeholder="Search"
           id="cityNameInput"
+          placeholder="Search Weather"
           type="text"
           required
           minLength={2}
@@ -43,6 +85,7 @@ const Home = () => {
           //   onChange={(e) => setCityName(e.target.value)}
           ref={cityNameRef}
         />
+        <br />
         <button type="submit"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.5em"
           width="1.5em" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -51,18 +94,17 @@ const Home = () => {
         </svg></button>
       </form>
 
+      <hr />
 
-      </div>
+      {isLoading ? <div>Loading...</div> : null}
 
-      <br /> <br /> <br />
+      {weatherData.length || currentLocationWeather || isLoading ? null : <div>No Data</div>}
 
-      {weatherData.length ? (
-        weatherData.map((eachWeatherData, index) => {
-          return <WeatherCard key={index} weatherData={eachWeatherData} />;
-        })
-      ) : (
-        <div>No Data</div>
-      )}
+      {weatherData.map((eachWeatherData, index) => {
+        return <WeatherCard key={index} weatherData={eachWeatherData} />;
+      })}
+
+      {currentLocationWeather ? <WeatherCard weatherData={currentLocationWeather} /> : null}
     </div>
   );
 };
--- src/component/weatherWidget/weatherWidget.jsx
+++ src/component/weatherWidget/weatherWidget.jsx
@@ -3,20 +3,17 @@ import "./weatherWidget.css";
 const WeatherCard = ({ weatherData }) => {
   return (
     <div className="card">
-      <h3> {weatherData?.name} {weatherData?.sys?.country}</h3>
-
-      {/* <h3>Country: {weatherData?.sys?.country}</h3> */}
-
-      <h2 className="temp">{weatherData?.main?.temp}°C</h2>
-
-      <div>Humidity: {weatherData?.main?.humidity}%</div>
-
-      <div>Wind Speed: {weatherData?.wind?.speed}km/h</div>
-
-      <div className="flex">Weather:
-        <img src="https://openweathermap.org/img/wn/04n.png" alt="" class="icon" />
-        {weatherData?.weather[0]?.description}
-      </div>
+      city name: {weatherData?.name}
+      <br />
+      country: {weatherData?.sys?.country}
+      <br />
+      <div className="temp">{weatherData?.main?.temp}°C</div>
+      <br />
+      humidity: {weatherData?.main?.humidity}
+      <br />
+      wind speed: {weatherData?.wind?.speed}
+      <br />
+      weather: {weatherData?.weather[0]?.description}
     </div>
   );
 };
